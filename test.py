76efded0a004277c5fd53cc6c8b79900138c2a10
tommaso seneci desktop <ttoommxx@gmail.com>
2023-02-06 01:40:53 +0000

    remove test file

--- test.py
+++
@@ -1,3 +0,0 @@
-dic = {'README.md': '2023-02-05 23:33:50', 'pyleManager.py': '2023-02-06 01:06:56', 'settings.py': '2023-02-06 01:06:48', '.gitignore': '2023-02-05 17:44:29'}
-print(sorted(dic.items(), key=lambda x:x[1]))
-print(dic)
