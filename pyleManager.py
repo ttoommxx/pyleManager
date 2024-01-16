0cd3553147f6bda1928ea371be97c75017544889
ttoommxx <ttoommxx@gmail.com>
2024-01-16 13:31:00 +0100

    pylinted

--- pyleManager.py
+++ pyleManager.py
@@ -1,9 +1,14 @@
 # REQUIRED MODULES
-import os, sys, time, argparse, itertools
+import os
+import sys
+import time
+import argparse
+import itertools
 from platform import system
 
 if os.name == "posix":
-    import termios, tty
+    import termios
+    import tty
 elif os.name == "nt":
     from msvcrt import getch as getch_encoded
 else:
@@ -11,30 +16,32 @@ else:
 
 
 # ARGUMENT PARSER
-parser = argparse.ArgumentParser(prog="pyleManager", description="file manager written in Python")
-parser.add_argument("-p", "--picker", action="store_true", help="use pyleManager as a file selector")
+parser = argparse.ArgumentParser(prog="pyleManager",
+                                 description="file manager written in Python")
+parser.add_argument("-p", "--picker",
+                    action="store_true",
+                    help="use pyleManager as a file selector")
 args = parser.parse_args() # args.picker contains the modality
 picker = args.picker
 
 
 # GLOBAL VARIABLES
-local_folder = os.path.abspath(os.getcwd()) # save original path
-dimension = False 
-time_modified = False
-hidden = False
-beep = False
-permission = False
-index = 0  # dummy index
-order = 0
-current_directory = None
-from_file = 0
-rows_len = os.get_terminal_size().lines
-instruction_string = None
-selection = None
+LOCAL_FOLDER = os.path.abspath(os.getcwd()) # save original path
+DIMENSION = False
+TIME_MODIFIED = False
+HIDDEN = False
+BEEP = False
+PERMISSION = False
+INDEX = 0
+ORDER = 0
+CURRENT_DIRECTORY = None
+START_LINE_DIRECTORY = 0
+ROWS_LENGTH = os.get_terminal_size().lines
+SELECTION = None
 
 
-# RETURN FILE SIZE AS A STRING
-def file_size(path):
+def file_size(path: str) -> str:
+    """ return file size as a formatted string """
     size = os.lstat(path).st_size
     i = len(str(size)) // 3
     if len(str(size)) % 3 == 0:
@@ -45,88 +52,91 @@ def file_size(path):
     return f'{size:.2f}{("b","kb","mb","gb")[i]}'
 
 
-# UPDATE ORDER, 0 stay 1 next
-def order_update(j):
-    global order
-    old_order = order
-    # create a vector with (1,a,b) where a,b are one if dimension and time_modified are enabled
+def order_update(stay: bool) -> None:
+    """ update order, False stay, True move to the next entry """
+    global ORDER
+    old_order = ORDER
+    # create a vector with (1,a,b) where a,b are one if dimension and TIME_MODIFIED are enabled
     vec = (1,
-           dimension * any(os.path.isfile(x) for x in directory()),
-           time_modified * any(os.path.isfile(x) for x in directory())
+           DIMENSION * any(os.path.isfile(x) for x in directory()),
+           TIME_MODIFIED * any(os.path.isfile(x) for x in directory())
            )
     # search the next 1 and if not found return 0
-    order = vec.index(1,order+j) if 1 in vec[order+j:] else 0
-    if order != old_order:
+    ORDER = vec.index(1, ORDER+stay) if 1 in vec[ORDER+stay:] else 0
+    if ORDER != old_order:
         # only update if the previous order was changed
-        global current_directory
-        current_directory = None
+        global CURRENT_DIRECTORY
+        CURRENT_DIRECTORY = None
     
 
-# LIST OF FOLDERS AND FILES
-def directory():
+def directory() -> list[str]:
+    """ list of folders and files """
     # return the previous value if exists
-    global current_directory
-    if current_directory is None:
+    global CURRENT_DIRECTORY
+    if CURRENT_DIRECTORY is None:
         directories = os.listdir()
         # order by
-        match order:
+        match ORDER:
             # size
             case 1:
                 dirs = list( itertools.chain( (x[0] for x in sorted({x:os.lstat(x).st_size for x in directories
-                                                           if os.path.isdir(x) and (hidden or not x.startswith(".") )}.items(), key=lambda x:x[1])),
+                                                                     if os.path.isdir(x) and (HIDDEN or not x.startswith("."))}.items(), key=lambda x: x[1])),
                                     (x[0] for x in sorted({x: os.lstat(x).st_size for x in directories
-                                                           if os.path.isfile(x) and (hidden or not x.startswith(".") )}.items(), key=lambda x:x[1])) ) )
+                                                           if os.path.isfile(x) and (HIDDEN or not x.startswith("."))}.items(), key=lambda x: x[1]))))
             # time modified
             case 2:
                 dirs = list( itertools.chain( (x[0] for x in sorted({x:os.lstat(x).st_mtime for x in directories
-                                                           if os.path.isdir(x) and (hidden or not x.startswith(".") )}.items(), key=lambda x:x[1])),
+                                                                     if os.path.isdir(x) and (HIDDEN or not x.startswith("."))}.items(), key=lambda x: x[1])),
                                     (x[0] for x in sorted({x: os.lstat(x).st_mtime for x in directories
-                                                           if os.path.isfile(x) and (hidden or not x.startswith(".") )}.items(), key=lambda x:x[1])) ) )
+                                                           if os.path.isfile(x) and (HIDDEN or not x.startswith("."))}.items(), key=lambda x: x[1]))))
             # name
             case _: # 0 and unrecognised values
                 dirs = list( itertools.chain( sorted((x for x in directories
-                                            if os.path.isdir(x) and (hidden or not x.startswith(".") ) ), key=lambda s: s.lower()),
+                                                      if os.path.isdir(x) and (HIDDEN or not x.startswith("."))), key=lambda s: s.lower()),
                                     sorted((x for x in directories
-                                            if os.path.isfile(x) and (hidden or not x.startswith(".") )), key=lambda s: s.lower()) ) )
-        current_directory = dirs
-    return current_directory
+                                            if os.path.isfile(x) and (HIDDEN or not x.startswith("."))), key=lambda s: s.lower())))
+        CURRENT_DIRECTORY = dirs
+    return CURRENT_DIRECTORY
 
 
 # CLEAN TERMINAL
 if os.name == "posix":
     def clear():
+        """ clear screen """
         os.system("clear")
 elif os.name == "nt":
     def clear():
+        """ clear screen """
         os.system("cls")
 
 
-# PRINTING FUNCTION
-def dir_printer(position = "beginning"):
-    global from_file, index
+def dir_printer(position:str = "beginning") -> None:
+    """ printing function """
+    global START_LINE_DIRECTORY
+    global INDEX
 
     # first check if I only have to print the index:
     if position == "up":
-        index -= 1
-        if index >= from_file:
+        INDEX -= 1
+        if INDEX >= START_LINE_DIRECTORY:
             # print up when we are in the range of visibility
             sys.stdout.write('\033[2A')
             print()
             return # exit the function
-        else:
-            # else print up one
-            from_file -= 1
-            position = "beginning" # return the cursor up
+        
+        # else print up one
+        START_LINE_DIRECTORY -= 1
+        position = "beginning" # return the cursor up
 
     elif position == "down":
-        index += 1
-        if index < rows_len - 3 + from_file:
+        INDEX += 1
+        if INDEX < ROWS_LENGTH - 3 + START_LINE_DIRECTORY:
             # print down when we are in the range of visibility
             print()
             return # exit the function
-        else:
-            # else print down 1
-            from_file += 1
+        
+        # else print down 1
+        START_LINE_DIRECTORY += 1
 
     clear()
     # length of columns
@@ -147,35 +157,34 @@ def dir_printer(position = "beginning"):
         l_size = max((len(file_size(x)) for x in directory()))
     
         # write the description on top
-        to_print.append( f" {'v' if order == 0 else ' '}*NAME*" )
+        to_print.append(f" {'v' if ORDER == 0 else ' '}*NAME*")
         columns = ""
-        if dimension and any(os.path.isfile(x) for x in directory()):
-            columns += f" |{'v' if order == 1 else ' '}*SIZE*{' '*(l_size-6)}"
-        if time_modified:
-            columns += f" |{'v' if order == 2 else ' '}*TIME MODIFIED*{' '*4}"
-        if permission: 
-            columns += f" | *PERM*"
+        if DIMENSION and any(os.path.isfile(x) for x in directory()):
+            columns += f" |{'v' if ORDER == 1 else ' '}*SIZE*{' '*(l_size-6)}"
+        if TIME_MODIFIED:
+            columns += f" |{'v' if ORDER == 2 else ' '}*TIME MODIFIED*{' '*4}"
+        if PERMISSION:
+            columns += " | *PERM*"
 
         to_print.append( f"{' '*(columns_len - len(columns)-8)}{columns}" )
 
         if position == "index":
-            index = min(index, len(directory())-1)
-            if index < rows_len - 3:
+            INDEX = min(INDEX, len(directory())-1)
+            if INDEX < ROWS_LENGTH - 3:
                 pass
             else:
-                from_file = index - (rows_len - 3) + 1
+                START_LINE_DIRECTORY = INDEX - (ROWS_LENGTH - 3) + 1
 
-        for x in itertools.islice(directory(), from_file, from_file + rows_len - 3):
+        for x in itertools.islice(directory(), START_LINE_DIRECTORY, START_LINE_DIRECTORY + ROWS_LENGTH - 3):
             to_print.append( f"\n {'<' if os.path.isdir(x) else ' '}" )
 
             # add extensions
             columns = ""
-            if dimension and os.path.isfile(x):
+            if DIMENSION and os.path.isfile(x):
                 columns += f" | {file_size(x)}{' '*(l_size - len(file_size(x)))}"
-            if time_modified:
+            if TIME_MODIFIED:
                 columns += f" | {time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(time.ctime(os.lstat(x).st_mtime)))}"
-            if permission:
-                permissions = os.stat(x).st_mode
+            if PERMISSION:
                 read_x = os.access(x, os.R_OK)
                 write_x = os.access(x, os.W_OK)
                 execute_x = os.access(x, os.X_OK)
@@ -187,17 +196,19 @@ def dir_printer(position = "beginning"):
     print("".join(to_print), end = "\r")
 
     if position == "beginning":
-        sys.stdout.write(f'\033[{ min(len(directory()), rows_len-3)  }A')
+        sys.stdout.write(f'\033[{ min(len(directory()), ROWS_LENGTH-3)  }A')
         print()
     elif position == "index":
-        if index < rows_len - 3:
-            sys.stdout.write(f'\033[{ min(len(directory()), rows_len-3) - index }A')
+        if INDEX < ROWS_LENGTH - 3:
+            sys.stdout.write(
+                f'\033[{min(len(directory()), ROWS_LENGTH-3) - INDEX}A')
             print()
 
 
 # FETCH KEYBOARD INPUT
 if os.name == "posix":
-    def getch():
+    def getch() -> str:
+        """ read raw terminal input """
         fd = sys.stdin.fileno()
         old_settings = termios.tcgetattr(fd)
         try:
@@ -208,7 +219,8 @@ if os.name == "posix":
         return ch
     
     conv_arrows = {"D":"left", "C":"right", "A":"up", "B":"down"}
-    def get_key():
+    def get_key() -> str:
+        """ process correct string for keyboard input """
         key_pressed = getch()
         match key_pressed:
             case "\r":
@@ -219,7 +231,8 @@ if os.name == "posix":
             case _:
                 return key_pressed
 elif os.name == "nt":
-    def getch():
+    def getch() -> str:
+        """ read raw terminal input """
         letter = getch_encoded()
         try:
             return letter.decode('ascii')
@@ -227,7 +240,8 @@ elif os.name == "nt":
             return letter
     
     conv_arrows = {"K":"left", "M":"right", "H":"up", "P":"down"}
-    def get_key():
+    def get_key() -> str:
+        """ process correct string for keyboard input """
         key_pressed = getch()
         match key_pressed:
             case "\r":
@@ -238,45 +252,42 @@ elif os.name == "nt":
                 return key_pressed
 
 
-# BEEP
-def beeper():
-    if beep:
+def beeper() -> None:
+    """ make a beep """
+    if BEEP:
         sys.stdout.write('\033[2A')
         print("\a\n")
 
 
-# RESET FOLDER SETTINGS
-def dir_printer_reset(refresh = False, restore_position = "beginning"):
-    global from_file, index, rows_len
+def dir_printer_reset(refresh:bool = False, restore_position:str = "beginning") -> None:
+    """ print screen after resetting directory attributes """
+    global START_LINE_DIRECTORY
+    global INDEX
+    global ROWS_LENGTH
     if refresh:
-        global current_directory
-        current_directory = None
+        global CURRENT_DIRECTORY
+        CURRENT_DIRECTORY = None
 
-    rows_len = os.get_terminal_size().lines
-    from_file = 0
+    ROWS_LENGTH = os.get_terminal_size().lines
+    START_LINE_DIRECTORY = 0
     if restore_position == "index":
         pass
     elif restore_position == "selection":
-        if selection in directory():
-            index = directory().index(selection)
+        if SELECTION in directory():
+            INDEX = directory().index(SELECTION)
         else:
-            index = 0
+            INDEX = 0
         restore_position = "index"
     else:
-        index = 0
+        INDEX = 0
     
     dir_printer(position = restore_position)
 
 
-# FILE MANAGER
-def main(*args):
-    global index, dimension, time_modified, from_file, hidden, rows_len, beep, instruction_string, permission, selection
-    
-    if args and args[0] in ("-p", "--picker"):
-        global picker
-        picker = True
-    
-    instruction_string = f"""INSTRUCTIONS:
+def instructions() -> None:
+    """ print instructions """
+    clear()
+    print( f"""INSTRUCTIONS:
 
 the prefix \"<\" means folder
 
@@ -286,46 +297,64 @@ rightArrow = open folder
 leftArrow = previous folder
 q = quit
 r = refresh
-h = toggle hidden files
-d = toggle file size
-t = toggle time last modified
-b = toggle beep
-p = toggle permission
-m = change ordering
+h = ({'yes' if HIDDEN else 'no'}) toggle hidden files
+d = ({'yes' if DIMENSION else 'no'}) toggle file size
+t = ({'yes' if TIME_MODIFIED else 'no'}) toggle time last modified
+b = ({'yes' if BEEP else 'no'}) toggle beep
+p = ({'yes' if PERMISSION else 'no'}) toggle permission
+m = ({("NAME", "SIZE", "TIME MODIFIED")[ORDER]}) change ordering
 enter = {
         'select file' if picker else 'open using the default application launcher'}
 e = {'--disabled--' if picker else 'edit using command-line editor'}
 
 def selection_permission(path):
 
-press any button to continue"""
+press any button to continue""", end = "")
+    get_key()
+
+
+def main(*args: list[str]) -> None:
+    """ file manager """
+    global INDEX
+    global DIMENSION
+    global TIME_MODIFIED
+    global START_LINE_DIRECTORY
+    global HIDDEN
+    global ROWS_LENGTH
+    global BEEP
+    global PERMISSION
+    global SELECTION
+    
+    if args and args[0] in ("-p", "--picker"):
+        global picker
+        picker = True
 
     dir_printer()
 
     while True:
         
         if len(directory()) > 0:
-            selection = directory()[index] # + file name if any
+            SELECTION = directory()[INDEX]  # + file name if any
 
         match get_key():
             # up
             case "up":
-                if len(directory()) > 0 and index > 0:
+                if len(directory()) > 0 and INDEX > 0:
                     dir_printer(position = "up")
                 else:
                     beeper()
 
             # down
             case "down":
-                if len(directory()) > 0 and index < len(directory())-1:
+                if len(directory()) > 0 and INDEX < len(directory())-1:
                     dir_printer(position = "down")
                 else:
                     beeper()
 
             # right
             case "right":
-                if len(directory()) > 0 and os.path.isdir(selection) and os.access(selection, os.R_OK):
-                    os.chdir(selection)
+                if len(directory()) > 0 and os.path.isdir(SELECTION) and os.access(SELECTION, os.R_OK):
+                    os.chdir(SELECTION)
                     dir_printer_reset(refresh=True, restore_position = "index")
                 else:
                     beeper()
@@ -341,52 +370,52 @@ press any button to continue"""
             # quit
             case "q":
                 clear()
-                os.chdir(local_folder)
+                os.chdir(LOCAL_FOLDER)
                 return
-            
+
             # refresh
             case "r":
                 dir_printer_reset(refresh = True, restore_position = "selection")
-            
+
             # toggle hidden
             case "h":
-                hidden = not hidden
+                HIDDEN = not HIDDEN
                 dir_printer_reset(refresh=True, restore_position="selection")
-            
+
             # size
             case "d":
-                dimension = not dimension
+                DIMENSION = not DIMENSION
                 dir_printer_reset(restore_position="selection")
 
             # time
             case "t":
-                time_modified = not time_modified
+                TIME_MODIFIED = not TIME_MODIFIED
                 dir_printer_reset(restore_position="selection")
 
             # beep
             case "b":
-                beep = not beep
+                BEEP = not BEEP
 
             # permission
             case "p":
-                permission = not permission
+                PERMISSION = not PERMISSION
                 dir_printer_reset(restore_position="selection")
 
             # change order
             case "m":
                 order_update(1)
                 dir_printer_reset(restore_position="selection")
-                
+
             # enter
             case "enter":
                 if len(directory()) > 0:
                     if picker:
-                        path = os.path.join(os.getcwd(), selection) #{os.sep if os.path.isdir(selection) else ''}"
+                        path = os.path.join(os.getcwd(), SELECTION)
                         clear()
-                        os.chdir(local_folder)
+                        os.chdir(LOCAL_FOLDER)
                         return path
                     elif not picker:
-                        selection_os = selection.replace("\"", "\\\"")
+                        selection_os = SELECTION.replace("\"", "\\\"")
                         match system():
                             case "Linux":
                                 os.system(f"xdg-open \"{selection_os}\"")
@@ -401,11 +430,11 @@ press any button to continue"""
                                 dir_printer_reset(restore_position="selection")
                 else:
                     beeper()
-            
+
             # command-line editor
             case "e":
                 if len(directory()) > 0 and not picker:
-                    selection_os = selection.replace("\"", "\\\"")
+                    selection_os = SELECTION.replace("\"", "\\\"")
                     match system():
                         case "Linux":
                             os.system(f"$EDITOR \"{selection_os}\"")
@@ -428,11 +457,9 @@ press any button to continue"""
 
             # instructions
             case "i":
-                clear()
-                print(instruction_string, end = "")
-                get_key()
+                instructions()
                 dir_printer_reset(restore_position="selection")
-                
+
             case _:
                 pass
 
